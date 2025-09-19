1fd0b85084e4b56852827e180eb7ac595c3f9a5a
Neale Upstone <neale@nealeupstone.com>
2025-09-19 21:00:13 +0100

    Add simple project with default wizard settings
    Just lombok and configuration-processor tasks ticked

---
+++ src/main/java/com/example/springbootgradle9logging/SpringBootGradle9LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgradle9logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradle9LoggingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootGradle9LoggingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springbootgradle9logging/SpringBootGradle9LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgradle9logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradle9LoggingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
